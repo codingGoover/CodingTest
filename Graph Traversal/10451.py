99be1b943fdb1a47799526c507bff3ebff8cc7b6
codingGoover <dpswpfgkxm30@naver.com>
2023-07-20 21:06:42 +0900

    [BFS] silver3 python solution

---
+++ Graph Traversal/10451.py
@@ -0,0 +1,91 @@
+'''
+10451번 순열사이클 실버3
+
+문제
+https://www.acmicpc.net/problem/10451
+
+입력
+첫째 줄에 테스트 케이스의 개수 T가 주어진다. 각 테스트 케이스의 첫째 줄에는 순열의 크기 N (2 ≤ N ≤ 1,000)이 주어진다. 
+둘째 줄에는 순열이 주어지며, 각 정수는 공백으로 구분되어 있다.
+
+출력
+각 테스트 케이스마다, 입력으로 주어진 순열에 존재하는 순열 사이클의 개수를 출력한다.
+
+예제 입력 1 
+2
+8
+3 2 7 8 1 4 5 6
+10
+2 1 3 4 5 6 7 9 10 8
+
+예제 출력 1 
+3
+7
+
+예제 입력 2
+6
+3
+1 2 3
+3
+3 2 1
+3
+2 1 3
+3
+3 1 2
+3
+1 3 2
+3
+2 3 1
+
+예제 출력 2
+3
+2
+2
+1
+2
+1
+
+'''
+
+from collections import deque
+import sys
+input=sys.stdin.readline
+
+def bfs(graph,visted,node):
+    global cycle
+    
+    visted[node]=True
+    now_node=node
+    
+    while True:
+        visted[now_node]=True
+        now_node=graph[now_node]
+        if now_node==node:
+            cycle+=1
+            break
+        if visted[now_node]==True:
+            break
+    
+    
+N= int(input())
+for _ in range(N):
+    cnt= int(input())
+    cycle=0
+    visted=[False]*(cnt+1)
+    graph=[0]
+    
+    graph.extend(map(int,input().split()))
+    #print(graph)
+    
+    for i in range(1,cnt+1):
+        if visted[i]==False:
+            if graph[i]==i:
+                cycle+=1
+                visted[i]=True
+            else:
+                bfs(graph,visted,i)
+    
+    print(cycle)
+    
+                
+    
---
+++ Graph Traversal/11725.py
@@ -0,0 +1,125 @@
+'''
+11725번 트리의 부모 찾기 실버2
+
+문제
+https://www.acmicpc.net/problem/11725
+
+입력
+첫째 줄에 노드의 개수 N (2 ≤ N ≤ 100,000)이 주어진다. 둘째 줄부터 N-1개의 줄에 트리 상에서 연결된 두 정점이 주어진다.
+
+출력
+첫째 줄부터 N-1개의 줄에 각 노드의 부모 노드 번호를 2번 노드부터 순서대로 출력한다.
+
+예제 입력 1 
+7
+1 6
+6 3
+3 5
+4 1
+2 4
+4 7
+
+예제 출력 1 
+4
+6
+1
+3
+1
+4
+
+예제 입력 2 
+12
+1 2
+1 3
+2 4
+3 5
+3 6
+4 7
+4 8
+5 9
+5 10
+6 11
+6 12
+
+예제 출력 2 
+1
+1
+2
+3
+3
+4
+4
+5
+5
+6
+6
+
+예제 입력3
+3
+2 3
+1 2
+
+예제 출력3
+1
+2
+
+예제 입력4
+10
+1 3
+5 4
+3 2
+2 7
+5 7
+9 10
+5 10
+6 8
+1 6
+
+예제 출력4
+3
+1
+5
+7
+1
+2
+6
+10
+5
+'''
+
+from collections import deque
+import sys
+input=sys.stdin.readline
+
+def bfs(graph,visted,node):
+    visted[node]=True
+    queue=deque([])
+    queue.append(node)
+    
+    while queue:
+        now_node=queue.popleft()
+        
+        for v in graph[now_node]:
+            if visted[v]==False:
+                queue.append(v)
+                visted[v]=True
+            else:
+                parent[now_node]=v
+ 
+
+
+
+N= int(input())
+visted=[False]*(N+1)
+parent=[0]*(N+1)
+graph=[[] for _ in range(N+1)]
+
+for _ in range(N-1):
+    v1,v2= map(int,input().split())
+    graph[v1].append(v2)
+    graph[v2].append(v1)
+    
+bfs(graph,visted,1)
+
+for p in parent[2:]:
+    print(p)
